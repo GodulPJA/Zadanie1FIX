534b3609fb0329df51c4a542d4d95890d0f8c6f1
Godul <s29252@pjwstk.edu.pl>
2026-03-23 21:28:49 +0100

    dodanie klasy Rental

---
+++ Zadanie1FIX/Rental.cs
@@ -0,0 +1,16 @@
+﻿namespace Zadanie1FIX;
+
+public class Rental
+{
+    //todo sprawdzić jakiej zmiennej daty użyć
+    
+    Tool atool;
+    public Guid Id { get; set; }
+    public User User { get; set; }
+    public Rental(Tool tool1,User user)
+    {
+        atool = tool1;
+        Id = Guid.NewGuid();
+        User = user;
+    }
+}
